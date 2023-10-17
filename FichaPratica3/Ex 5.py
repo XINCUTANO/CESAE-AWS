9ac34389d503505f1287c060fb34623ac7161086
Vasco Santos <svasco6@gmail.com>
2023-10-17 16:48:31 +0100

    FichaPratica3

---
+++ FichaPratica3/Ex 5.py
@@ -0,0 +1,10 @@
+print("Insira o numero inicial: ")
+numinicial=int(input())
+print("Insira o numero final:")
+numfinal=int(input())
+
+while(numinicial<=numfinal):
+    print(numinicial)
+    numinicial=numinicial+1
+    
+
---
+++ FichaPratica3/Ex 6.py
@@ -0,0 +1,8 @@
+num=1
+somatorio=0
+
+while(num<=100):
+    print(num)
+    somatorio=somatorio+num
+    num+=1
+print("Somatorio:",somatorio)
